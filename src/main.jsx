c586eb9d3bff1b61166fa87a5146c691c21e0445
Pathan623 <msaadkhanpathan0786@gmail.com>
2026-07-31 13:45:09 +0530

    fix: remount CustomerMenu on table number change to prevent stale state

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, useParams } from 'react-router-dom'
 import CustomerMenu from './pages/CustomerMenu.jsx'
 import KitchenDashboard from './pages/KitchenDashboard.jsx'
 import AdminPanel from './pages/AdminPanel.jsx'
 import './index.css'
 
+function CustomerMenuWithKey() {
+  const { tableNumber } = useParams()
+  return <CustomerMenu key={tableNumber} />
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/menu/:tableNumber" element={<CustomerMenu />} />
+        <Route path="/menu/:tableNumber" element={<CustomerMenuWithKey />} />
         <Route path="/kitchen" element={<KitchenDashboard />} />
         <Route path="/admin" element={<AdminPanel />} />
         <Route path="*" element={<Navigate to="/admin" replace />} />
